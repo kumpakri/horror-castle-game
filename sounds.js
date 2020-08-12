09f29257ef2323ba2188bac174f95b864dcb1ade
Kristyna Kumpanova <kumpakri@gmail.com>
2020-08-12 21:00:56 +0200

    Sounds moved together to one script.

--- sounds.js
+++ sounds.js
@@ -2,9 +2,199 @@ var rainSound = new Howl({
     src: ['sound/rain.wav'],
     loop: true,
     volume: 0.5
-  });
-  var outdoorStepsSound = new Howl({
+});
+var roomSound = new Howl({
+    src: ['sound/horror-ambience.flac'],
+    loop: true,
+    volume: 0.5
+});
+var silenceSound = new Howl({
+    src: ['sound/silence.wav'],
+    loop: true,
+    volume: 1.0
+});
+var outdoorStepsSound = new Howl({
     src: ['sound/steps.wav'],
     loop: false,
     volume: 0.5
-  });
+});
+var stoneStepsSound = new Howl({
+    src: ['sound/stone_steps.mp3'],
+    loop: false,
+    volume: 0.5
+});
+var stairsSound = new Howl({
+    src: ['sound/stair-steps.mp3'],
+    loop: false,
+    volume: 0.5
+});
+var creakingDoorSound = new Howl({
+    src: ['sound/door-creak.wav'],
+    loop: false,
+    volume: 0.5
+});
+var openDoorSound = new Howl({
+    src: ['sound/small_door_open.wav'],
+    loop: false,
+    volume: 1.0
+});
+var slideDoorSound = new Howl({
+    src: ['sound/door_open.wav'],
+    loop: false,
+    volume: 1.0
+});
+var knockAndOpenDoorSound = new Howl({
+    src: ['sound/kocking-door-and-open-door.mp3'],
+    loop: false,
+    volume: 0.8
+});
+var doorSlapSound = new Howl({
+    src: ['sound/door-knock.wav'],
+    loop: false,
+    volume: 1.0
+});
+var openCabinetSound = new Howl({
+    src: ['sound/cabinet-door-open.wav'],
+    loop: false,
+    volume: 1.0
+});
+var closeCabinetSound = new Howl({
+    src: ['sound/cabinet-close.wav'],
+    loop: false,
+    volume: 0.1
+});
+var drawerSound = new Howl({
+    src: ['sound/drawer-open.wav'],
+    loop: false,
+    volume: 1.0
+});
+var closeDrawerSound = new Howl({
+    src: ['sound/drawer-close.wav'],
+    loop: true,
+    volume: 0.5
+});
+var doorShutSound = new Howl({
+    src: ['sound/door-shut.wav'],
+    loop: false,
+    volume: 1.0
+});
+var closeDoorSound = new Howl({
+    src: ['sound/close-door.wav'],
+    loop: false,
+    volume: 1.0
+});
+var lockedDoorSound = new Howl({
+    src: ['sound/door-locked.wav'],
+    loop: false,
+    volume: 1.0
+});
+var windowSound = new Howl({
+    src: ['sound/door-squeek.wav'],
+    loop: false,
+    volume: 0.5
+});
+var padlockSound = new Howl({
+    src: ['sound/padlock.flac'],
+    loop: false,
+    volume: 0.5
+});
+var carpetSound = new Howl({
+    src: ['sound/carpet.mp3'],
+    loop: false,
+    volume: 0.5
+});
+var boxSound = new Howl({
+    src: ['sound/open-box.wav'],
+    loop: false,
+    volume: 0.5
+});
+var drippingSound = new Howl({
+    src: ['sound/drip.wav'],
+    loop: true,
+    volume: 1.0
+});
+var bonesSound = new Howl({
+    src: ['sound/bones.wav'],
+    loop: false,
+    volume: 0.5
+});
+var vultureSound = new Howl({
+    src: ['sound/vulture.wav'],
+    loop: false,
+    volume: 0.5
+});
+var lidSound = new Howl({
+    src: ['sound/pot-lid.mp3'],
+    loop: false,
+    volume: 0.5
+});
+var pokeSound = new Howl({
+    src: ['sound/poke1.wav'],
+    loop: false,
+    volume: 1.0
+});
+var pianoSound = new Howl({
+    src: ['sound/piano.wav'],
+    loop: false,
+    volume: 0.5
+});
+var ghostSound = new Howl({
+    src: ['sound/ghost.wav'],
+    loop: false,
+    volume: 0.1
+});
+var mouseSound = new Howl({
+    src: ['sound/mouse-squeek.wav'],
+    loop: false,
+    volume: 0.5
+});
+var pictureSound = new Howl({
+    src: ['sound/sliding-wood.wav'],
+    loop: false,
+    volume: 1.0
+});
+var screamSound = new Howl({
+    src: ['sound/scream.wav'],
+    loop: false,
+    volume: 0.5
+});
+var mouseSqeakSound = new Howl({
+    src: ['sound/mouse-squeek2.wav'],
+    loop: false,
+    volume: 0.5
+});
+var boilingSound = new Howl({
+    src: ['sound/boiling-porridge.wav'],
+    loop: true,
+    volume: 0.5
+});
+var mouseEatingSound = new Howl({
+    src: ['sound/mouse-eating.wav'],
+    loop: true,
+    volume: 0.5
+});
+var laughSound = new Howl({
+    src: ['sound/child-laughs.wav'],
+    loop: false,
+    volume: 0.1
+});
+var childSound = new Howl({
+    src: ['sound/creepy-child.mp3'],
+    loop: false,
+    volume: 0.1
+});
+var hummSound = new Howl({
+    src: ['sound/humming.wav'],
+    loop: false,
+    volume: 0.1
+});
+var songSound = new Howl({
+    src: ['sound/sad-song.mp3'],
+    loop: false,
+    volume: 0.1
+});
+var doorSlamSound = new Howl({
+    src: ['sound/door-slam.wav'],
+    loop: false,
+    volume: 0.5
+});
